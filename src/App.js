39428e1742a0fddcbf8fa35418d01719e5ade5da
CatoG <47473856+CatoG@users.noreply.github.com>
2026-02-02 16:15:28 +0100

    starting instant booking

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Landing_Page from "./Components/Landing_Page/Landing_Page";
 import Login from "./Components/Login/Login";
 import Sign_Up from "./Components/Sign_Up/Sign_Up";
+import Instant_Consultation from "./Components/InstantConsultationBooking/InstantConsultation";
 
 // Function component for the main App
 function App() {
